7c0cde562493cdc9542a916075017f580e70170d
Drauwood <drauwood@gmail.com>
2026-06-15 00:22:44 +0300

    Enable rust_analyzer and tombi in LSP setup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,8 +16,7 @@ return {
                 ensure_installed = {
                     'lua_ls',
                     'clangd',
-                    -- 'rust_analyzer', 'tombi',
-                    'csharp-language-server'
+                    'rust_analyzer', 'tombi'
                 }
             })
         end
