adb1d8a6c764e0c910d58c2f445439b5618d19a0
Drauwood <drauwood@gmail.com>
2026-06-28 07:51:28 +0300

    Disable lua_ls in LSP setup
    Comment out 'lua_ls' in the LSP configuration.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -14,7 +14,7 @@ return {
         config = function ()
             require('mason-lspconfig').setup({
                 ensure_installed = {
-                    'lua_ls',
+                    -- 'lua_ls',
                     'clangd'
                     -- 'rust_analyzer', 'tombi'
                 }
